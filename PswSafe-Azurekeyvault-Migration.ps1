e1c6e9c32561d4125de5be61f83951a1e0d9f951
Vembarasan Thambidurai <37688145+vembu0806@users.noreply.github.com>
2022-04-01 00:17:35 +0530

    Update and rename PswSafe-keyvault-MIG-001.ps1 to PswSafe-Azurekeyvault-Migration.ps1

--- PswSafe-Azurekeyvault-Migration.ps1
+++ PswSafe-Azurekeyvault-Migration.ps1
@@ -5,6 +5,5 @@ $Title = $_.Title;
 $ContentType = $_.Username;
 $Password = convertto-securestring -string $_.Password -asplaintext -force
 Set-AzKeyVaultSecret -VaultName "Keyvault name" -Name $_.Title -SecretValue $Password -ContentType $ContentType
-
 }
 #Get-AzKeyVaultSecret -VaultName "Keyvault name" | select ContentType | sort ContentType
